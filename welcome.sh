d8614e53b5e1103bd8de3fb5a5f05a720172e9bf
Grigoriy Safronov <gvsafronov@yandex.ru>
2019-12-21 19:26:53 +0300

    Update welcome.sh

--- welcome.sh
+++ welcome.sh
@@ -2,6 +2,5 @@
 echo 'Try editing the markdown file in samples/welcome and see the'
 echo 'results instantly in the browser at http://localhost:3000/'
 echo '\n'
-cd flunix
 mv algernon flunix
 ./flunix --dev --conf serverconf.lua --dir samples/welcome --httponly --debug --autorefresh --bolt --server "$@"
